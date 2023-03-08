dcbfa9c03cbb051caff5f1dec474c891a08129c8
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-07 23:13:59 -0800

    Cleaned up indentation, spacing, and semicolons.

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -6,15 +6,15 @@ const eqArrays = function (arr1, arr2) {
     }
   }
   return true;
-}
+};
 
-const assertArrayEqual = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   const isEqual = eqArrays(arr1, arr2);
-  return isEqual 
-  ? console.log(`🟢Assertion Passed: ${arr1} === ${arr2}`) 
-  : console.log(`🔴Assertion Failed: ${arr1} !== ${arr2}`);
-}
+  return isEqual
+    ? console.log(`🟢Assertion Passed: ${arr1} === ${arr2}`)
+    : console.log(`🔴Assertion Failed: ${arr1} !== ${arr2}`);
+};
 
-assertArrayEqual([3, 2, 1], [3, 2, 1, 8]); 
-assertArrayEqual([3, 2, 1], [3, 2, 1]); 
-assertArrayEqual([3, 2, 1], [1, 2, 3]); 
+assertArraysEqual([3, 2, 1], [3, 2, 1, 8]);
+assertArraysEqual([3, 2, 1], [3, 2, 1]);
+assertArraysEqual([3, 2, 1], [1, 2, 3]);

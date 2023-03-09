f5a6033a11fb01335d548b4f160c2f6278ae6b8e
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-09 00:43:17 -0800

    Added a conditional statement that checks whether the arrays being compared are the same length.

--- eqArray.js
+++ eqArray.js
@@ -1,4 +1,4 @@
-// Assert function that checks for strict equally between two parameters.
+// Assert function that checks for strict equality between two parameters.
 
 const assertEqual = function(actual, expected) {
   return actual === expected
@@ -6,26 +6,27 @@ const assertEqual = function(actual, expected) {
     : console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// A function that checks the equality of two separate arrays. A for-loop 
-// is used to loop through the first array, which compares whether the 
-// index value of the first array is strictly equally to the same index 
-// value of the second array. Returns 'true' if all values are equal. 
+// A function that checks the equality of two separate arrays. A for-loop
+// is used to loop through the first array, which compares whether the
+// index value of the first array is strictly equal to the same index
+// value of the second array.
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
-}
+};
 
 // Calling the eqArrays function as the first argument in the assertEqual
 // function allows us to check the result of our comparison of arrays against
-// an expected outcome. 
+// an expected outcome.
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); 
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);

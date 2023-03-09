18524ba282471d1e5a590c5269ea5559da9a3a43
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-09 00:49:57 -0800

    Removed console.log() used for testing.

--- eqObjects.js
+++ eqObjects.js
@@ -24,7 +24,6 @@ const eqObjects = function (object1, object2) {
     if (Array.isArray(object1[key], object2[key])) {
       if (!eqArrays(object1[key], object2[key])) return false;
     } else if (object1[key] !== object2[key]) {
-      console.log(object1[key], object2[key]);
       return false;
     };
   };

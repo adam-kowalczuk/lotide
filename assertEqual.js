c23dab99041e4435b69379e4d1bc31beb455b3c9
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 13:30:10 -0700

    Removed test code and added code for exporting function.

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,7 @@
 const assertEqual = function(actual, expected) {
   return actual === expected
-    ? console.log(`🟢Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
+    ? `🟢Assertion Passed: ${actual} === ${expected}`
+    : `🔴Assertion Failed: ${actual} !== ${expected}`;
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(2, 1);
-assertEqual("Hello World", "Hello World");
+module.exports = assertEqual;

dd698598fb4665d4a9bd3d5f8fee1d9140c0702c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 14:05:25 -0700

    Removed test code and added code to export the function.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,12 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(arr1, arr2) {
-  //A variable with the value of whether two arrays are equal, as compared using the helper function eqArrays
+  //A variable with the return value of eqArrays, a function that evaulates whether two arrays are equal
   const isEqual = eqArrays(arr1, arr2);
   //Testing whether the result of isEqual matches an assertion
   return isEqual
-    ? console.log(`🟢Assertion Passed: ${arr1} === ${arr2}`)
-    : console.log(`🔴Assertion Failed: ${arr1} !== ${arr2}`);
+    ? `🟢Assertion Passed: ${arr1} === ${arr2}`
+    : `🔴Assertion Failed: ${arr1} !== ${arr2}`;
 };
 
-assertArraysEqual([3, 2, 1], [3, 2, 1, 8]);
-assertArraysEqual([3, 2, 1], [3, 2, 1]);
-assertArraysEqual([3, 2, 1], [1, 2, 3]);
+module.exports = assertArraysEqual;

16cec188d64fd3858d01be1740aa6e698844e4d3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 13:31:26 -0700

    Removed test code and added code for exporting function.

--- head.js
+++ head.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  return actual === expected
-    ? console.log(`🟢Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const head = function(arr) {
   return arr[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined);
+module.exports = head;

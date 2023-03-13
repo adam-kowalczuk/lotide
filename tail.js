de42ab33e4356bcf547859e367df1b8f2de0d19b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 13:38:55 -0700

    Removed test code and added code to export function.

--- tail.js
+++ tail.js
@@ -1,17 +1,12 @@
-const assertEqual = function(actual, expected) {
-  return actual === expected
-    ? console.log(`🟢Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const tail = function(arr) {
-  if (!arr.length) return [];
-  if (arr.length === 1) return [];
+  if (!arr.length) {
+    return []};
+  if (arr.length === 1) {
+    return []};
   return arr.slice(1);
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+
+
+module.exports = tail;
 

a6a173b90dcc94b0ec633a216cdddc81c03c69dc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 15:06:46 -0700

    Refactored code to utilize mocha and chai.

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,18 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns [] if input array is empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("returns [] if input array only has one element", () => {
+    assert.deepEqual(tail(["Hello"]), []);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+});
+
+
 console.log(tail(["Hello", "Lighthouse", "Labs"]));
+

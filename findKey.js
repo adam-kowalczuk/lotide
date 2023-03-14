795ee3f3e41ff89fc9a07939836acbb5ada3a786
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 22:19:15 -0700

    Added a prose description about purpose of function, as well as test code.

--- findKey.js
+++ findKey.js
@@ -1,7 +1,9 @@
+//Utilizes a callback method to look for a matching value in an object, and returns the key of that value. 
+
 const findKey = function (obj, callback) {
   //Loops through object passed into the first arugment
   for (const key in obj) {
-    //If the value of the key passed into the callback function returns true
+    //If the value of the key passed into the callback function returns true,
     if (callback(obj[key])) {
       //Return the first key that matches 
       return key;
@@ -12,3 +14,24 @@ const findKey = function (obj, callback) {
 }
 
 module.exports = findKey;
+
+//TESTS
+
+// const assertEqual = require('./assertEqual');
+
+// const michelinRestaurants = {
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri": { stars: 3 },
+//   "noma": { stars: 2 },
+//   "elBulli": { stars: 3 },
+//   "Ora": { stars: 2 },
+//   "Akelarre": { stars: 3 }
+// }
+
+// console.log(findKey(michelinRestaurants, x => x.stars === 2)); // -> 'noma'
+// console.log(findKey(michelinRestaurants, x => x.stars === 5)); // -> 'undefined' (There is no such thing as a Michelin 5-starred restaurant (yet!))
+
+// const twoStarredRestaurant = findKey(michelinRestaurants, x => x.stars === 2);
+
+// assertEqual(twoStarredRestaurant, 'noma'); // -> Passed
+// assertEqual(twoStarredRestaurant, 'Ora'); // -> Failed

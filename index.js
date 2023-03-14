efd423cf7a7c07e7162a18c7a4f8942676d9a447
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 18:31:00 -0700

    Added eqArrays, findKey, findKeyByValue, and flatten functions to index.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const assertArraysEqual = require('./assertArraysEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten')
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
@@ -13,6 +17,10 @@ module.exports = {
   assertObjectsEqual: assertObjectsEqual,
   countLetters: countLetters,
   countOnly: countOnly,
+  eqArrays: eqArrays,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
   head: head,
   tail: tail,
   middle: middle

0a9a44f791505201be54ed167520e4547e052a03
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 22:14:22 -0700

    Added comment stating purpose of the function, as well as test code.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,5 @@
+//Hunts through an object for a matching value, and returns that values key (return undefined if value does not exist in object)
+
 const findKeyByValue = function(obj, value) {
   for (let key in obj) {
     if (obj[key] === value) {
@@ -8,3 +10,16 @@ const findKeyByValue = function(obj, value) {
 };
 
 module.exports = findKeyByValue;
+
+//TESTS
+
+const assertEqual = require('./assertEqual');
+
+const bestTVShowsByGenre = {
+  sci_fi: "Blade",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

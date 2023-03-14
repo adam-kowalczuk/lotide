b5f81d5f1c6cee3a9e569e3f1ee29c796a91bf1c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 22:24:32 -0700

    Added a description about purpose of function, as well as test code.

--- countOnly.js
+++ countOnly.js
@@ -1,4 +1,6 @@
-//Determine whether an item in an object (itemsToCount) should be in an array, and if so, then count how many times that item objects in said array. 
+//Determine whether an item in an object (itemsToCount) should be in an array, 
+// and if so, then count how many times that item appears in said array. If 
+// desired item is not in the array, 'undefined' is returned.
 
 const countOnly = function (allItems, itemsToCount) {
   const results = {}
@@ -6,7 +8,7 @@ const countOnly = function (allItems, itemsToCount) {
   for (const item of allItems) {
     if (itemsToCount[item]) { 
       if (results[item]) {
-        results[item] += 1;
+        results[item]++;
       } else {
         results[item] = 1;
       }
@@ -17,3 +19,27 @@ const countOnly = function (allItems, itemsToCount) {
 };
 
 module.exports = countOnly;
+
+//TESTS
+
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
+
+// const assertEqual = require('./assertEqual');
+
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+// assertEqual(result1["Jason"], 1);
+// assertEqual(result1["Karima"], undefined);
+// assertEqual(result1["Fang"], 2);
+// assertEqual(result1["Agouhanna"], undefined);
+

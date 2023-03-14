7626791af161ff90f7a063ba839219f81ccc7b82
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 18:13:59 -0700

    Added assertEqual, assertArraysEqual, assertObjectsEqual, countLetters, and countOnly to index.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const assertEqual = require('./assertEqual');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
 
 module.exports = {
+  assertEqual: assertEqual,
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
   head: head,
   tail: tail,
   middle: middle

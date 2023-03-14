bc6ee2e64e76aa81acff9f228c3ab0d991b54ed5
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 21:38:06 -0700

    Added test code to file.

--- map.js
+++ map.js
@@ -1,7 +1,8 @@
+const assertArraysEqual = require('./assertArraysEqual');
 //Loops through an array, applying a callback function to each item, and then returns an array with each altered item. 
 const map = function(array, callback) {
   const results = [];
-  //For-of-loop that loops through the array passed in as the first argument.
+  //Loops through the array passed in as the first argument.
   for (let item of array) {
     //Each item is passed to the callback function, then pushed to the result array. 
     results.push(callback(item));
@@ -9,4 +10,14 @@ const map = function(array, callback) {
   return results;
 };
 
-module.exports = map;
+const words = ["ground", "control", "to", "major", "tom"];
+const numbers = [1, 2, 3, 4, 5, 6];
+const numStr = ["1", "2", "3", "4", "5", "6"]
+
+const results1 = map(words, word => word[0]);
+const results2 = map(numbers, number => number * 2);
+const results3 = map(numStr, str => Number(str));
+
+assertArraysEqual((results1), ['g', 'c', 't', 'm', 't']);
+assertArraysEqual((results2), [ 2, 4, 6, 8, 10, 12 ]);
+assertArraysEqual((results3), [ 1, 2, 3, 4, 5, 6 ]);

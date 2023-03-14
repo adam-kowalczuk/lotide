af2bac60e1fed9b5832c7f6c9b25c8b9f0e076da
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 21:44:32 -0700

    Edited and commented-out test.

--- map.js
+++ map.js
@@ -13,16 +13,17 @@ const map = function(array, callback) {
 module.exports = map;
 
 //TESTS
-const assertArraysEqual = require('./assertArraysEqual');
 
-const words = ["ground", "control", "to", "major", "tom"];
-const numbers = [1, 2, 3, 4, 5, 6];
-const numStr = ["1", "2", "3", "4", "5", "6"]
+// const assertArraysEqual = require('./assertArraysEqual');
 
-const results1 = map(words, word => word[0]);
-const results2 = map(numbers, number => number * 2);
-const results3 = map(numStr, str => Number(str));
+// const words = ["ground", "control", "to", "major", "tom"];
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const numStr = ["1", "2", "3", "4", "5", "6"]
 
-assertArraysEqual((results1), ['g', 'c', 't', 'm', 't']);
-assertArraysEqual((results2), [ 2, 4, 6, 8, 10, 12 ]);
-assertArraysEqual((results3), [ 1, 2, 3, 4, 5, 6 ]);
+// const results1 = map(words, word => word[0]);
+// const results2 = map(numbers, number => number * 2);
+// const results3 = map(numStr, str => Number(str));
+
+// assertArraysEqual((results1), ['g', 'c', 't', 'm', 't']);
+// assertArraysEqual((results2), [ 2, 4, 6, 8, 10, 12 ]);
+// assertArraysEqual((results3), [ 1, 2, 3, 4, 5, 6 ]);

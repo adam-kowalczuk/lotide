25fc02b84d02d8fa507896f223715b14d82962c3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 21:55:26 -0700

    Added assertion tests and refactored code for legibility.

--- without.js
+++ without.js
@@ -1,4 +1,4 @@
-//Removes items from an array, and returns array of remaining items. 
+//Removes desired items from an array, and returns array of remaining items. 
 
 const without = function(source, itemsToRemove) {
   let remainingItems = [];
@@ -7,8 +7,19 @@ const without = function(source, itemsToRemove) {
       remainingItems.push(item);
     }
   }
-  console.log(remainingItems);
+ return remainingItems;
 };
 
 module.exports = without;
 
+//TESTS
+
+// const assertArraysEqual = require('./assertArraysEqual');
+
+// const withoutColors = without(["red", "orange", "yellow"], ["yellow", "orange"]);
+// const withoutNumbersOne = without([1, 2, 3], [3]);
+// const withoutNumbersTwo = without(["1", "2", "3"], ["1", "2", 3]);
+
+// assertArraysEqual(withoutColors, ["red"]);
+// assertArraysEqual(withoutNumbersOne, [1, 2]);
+// assertArraysEqual(withoutNumbersTwo, ["3"]);

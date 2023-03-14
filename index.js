cf20c96b0a0fcab016f72a2230942bffada8f2a3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-03-13 19:00:50 -0700

    Added remaining utility functions.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
@@ -8,12 +8,17 @@ const findKey = require('./findKey');
 const findKeyByValue = require('./findKeyByValue');
 const flatten = require('./flatten')
 const head = require('./head');
-const tail = require('./tail');
+const letterPositions = require('./letterPositions');
+const map = require('./map')
 const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
-  assertEqual: assertEqual,
   assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
   assertObjectsEqual: assertObjectsEqual,
   countLetters: countLetters,
   countOnly: countOnly,
@@ -22,6 +27,10 @@ module.exports = {
   findKeyByValue: findKeyByValue,
   flatten: flatten,
   head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
   tail: tail,
-  middle: middle
+  takeUntil: takeUntil,
+  without: without
 };

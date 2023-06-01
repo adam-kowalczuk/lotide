03c50ea4ec8993f0a047edf55c9740beb69d924a
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-06-01 08:49:41 -0700

    Refactor function definition to use recursion

--- flatten.js
+++ flatten.js
@@ -1,30 +1,25 @@
 const flatten = function(arr) {
-  const flattenedArr = [];
-  // Loop through the array passed into the function
-  for (const element of arr) {
-    // If an element is an array,
-    if (Array.isArray(element)) {
-      // Loop through the element array,
-      for (const subElement of element) {
-        // And push its subElements to flattenedArr
-        flattenedArr.push(subElement);
-      }
-      // If element is NOT an array, simply push the current element to flattenedArr
+  let result = [];
+  arr.forEach((item) => {
+    //If item is an array
+    if (Array.isArray(item)) {
+      //Use recursion and concatenate it to result array
+      result = result.concat(flatten(item));
     } else {
-      flattenedArr.push(element);
+      result.push(item);
     }
-  }
-  return flattenedArr;
+  });
+  return result;
 };
 
 module.exports = flatten;
 
-
 //TESTS
 
-const assertArraysEqual = require('./assertArraysEqual');
+const assertArraysEqual = require("./assertArraysEqual");
 
 const arr1 = [1, 2, [3, 4], 5, [6]];
+
 console.log(flatten(arr1)); // -> [1, 2, 3, 4, 5, 6]
 
 assertArraysEqual(flatten(arr1), [1, 2, 3, 4, 5, 6]); // Assertion Passed
